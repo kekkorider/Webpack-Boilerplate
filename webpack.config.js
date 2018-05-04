f30c525575b6073939a9049f8a1153a8067bd333
Francesco Michelini <kekkorider@gmail.com>
2018-05-04 14:49:58 +0200

    Add ES Lint. Add inline SVG loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const path               = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin  = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin  = require('html-webpack-plugin');
-const UglifyJsPlugin     = require('uglifyjs-webpack-plugin');
+/* global module __dirname require */
+const path                       = require("path")
+const CleanWebpackPlugin         = require("clean-webpack-plugin")
+const ExtractTextPlugin          = require("extract-text-webpack-plugin")
+const HtmlWebpackPlugin          = require("html-webpack-plugin")
+const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin")
+const UglifyJsPlugin             = require("uglifyjs-webpack-plugin")
 
 module.exports = {
     entry: {
-        app: './src/index.js'
+        app: "./src/index.js"
     },
     output: {
-        filename: '[name].bundle.js', 
-        path: path.resolve(__dirname, 'dist')
+        filename: "[name].bundle.js", 
+        path: path.resolve(__dirname, "dist")
     }, 
-    devtool: 'source-map', 
+    devtool: "source-map", 
     devServer: {
-        contentBase: './dist'
+        contentBase: "./dist"
     }, 
     module: {
         rules: [
@@ -22,72 +24,75 @@ module.exports = {
                 test: /\.js$/, 
                 exclude: /node_modules/, 
                 use: {
-                    loader: 'babel-loader', 
+                    loader: "babel-loader", 
                     options: {
-                        presets: ['env']
+                        presets: ["env"]
                     }
                 }
             },
             {
-                test: /\.(jpg|png)$/, 
+                test: /\.(jpg|png|svg|gif)$/, 
                 exclude: /node_modules/, 
                 use: [
                     {
-                        loader: 'file-loader', 
+                        loader: "file-loader", 
                         options: {
-                            name: 'img/[name].[ext]'
+                            name: "img/[name].[ext]"
                         }
                     }, 
                     {
-                        loader: 'image-webpack-loader', 
+                        loader: "image-webpack-loader", 
                         options: {
                             optipng: {
                                 enabled: false
                             }, 
                             pngquant: {
                                 enabled: false, 
-                                quality: '100'
+                                quality: "100"
                             }
                         }
                     }
                 ]
             }, 
             {
-                test: /\.woff2$/, 
+                test: /\.(woff|woff2|ttf|eot)$/, 
                 exclude: /node_modules/, 
                 use: {
-                    loader: 'file-loader', 
+                    loader: "file-loader", 
                     options: {
-                        name: 'fonts/[name].[ext]'
+                        name: "fonts/[name].[ext]"
                     }
                 }
-            }, 
+            },
             {
                 test: /\.scss$/, 
                 exclude: /node_modules/, 
                 use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader', 
+                    fallback: "style-loader", 
                     use: [
-                        {loader: 'css-loader', options: {sourceMap: true}}, 
-                        {loader: 'postcss-loader', options: {sourceMap: true}}, 
-                        {loader: 'sass-loader', options: {sourceMap: true}}
+                        {loader: "css-loader", options: {sourceMap: true}}, 
+                        {loader: "postcss-loader", options: {sourceMap: true}}, 
+                        {loader: "sass-loader", options: {sourceMap: true}}
                     ]
                 })
             }
         ]
     }, 
     plugins: [
-        new CleanWebpackPlugin(['dist']), 
+        new CleanWebpackPlugin(["dist"]),
         new ExtractTextPlugin({
-            filename: '[name].bundle.css'
-        }), 
+            filename: "[name].bundle.css"
+        }),
         new HtmlWebpackPlugin({
             filename: "index.html", 
             template: "src/views/index.html", 
             title: "Webpack Boilerplate", 
             hash: true, 
-            chunks: ['app']
-        }), 
+            chunks: ["app"]
+        }),
+        new HtmlWebpackInlineSVGPlugin({
+            runPreEmit: true
+        }),
         new UglifyJsPlugin()
     ]
-};
+}
